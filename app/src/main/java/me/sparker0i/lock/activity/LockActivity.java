fb820cbe53018edc409f8d90dfe79f045253e572
Aaditya Menon <aadityamenon007@gmail.com>
2017-09-07 11:32:15 +0530

    Working Recent Button Block

--- app/src/main/java/me/sparker0i/lock/activity/LockActivity.java
+++ app/src/main/java/me/sparker0i/lock/activity/LockActivity.java
@@ -3,32 +3,36 @@ package me.sparker0i.lock.activity;
 import android.app.Activity;
 import android.app.ActivityManager;
 import android.app.KeyguardManager;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.PowerManager;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.WindowManager;
-import android.widget.Toast;
 
 import me.sparker0i.lawnchair.R;
 import me.sparker0i.lock.DBHelper;
-import me.sparker0i.lock.service.ScreenService;
 
 @SuppressWarnings("deprecation")
 public class LockActivity extends Activity implements OnClickListener{
 
     KeyguardManager km = null;
     KeyguardManager.KeyguardLock keyLock = null;
+    private Handler windowCloseHandler;
+    private Runnable windowCloserRunnable;
+    int task;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lock);
+        windowCloseHandler = new Handler();
+        task = getTaskId();
 
         if(km == null)
             km = (KeyguardManager) getSystemService(Context.KEYGUARD_SERVICE);
@@ -36,36 +40,19 @@ public class LockActivity extends Activity implements OnClickListener{
         if(keyLock == null)
             keyLock = km.newKeyguardLock(Context.KEYGUARD_SERVICE);
 
-        SQLiteDatabase db = new DBHelper(this).getWritableDatabase();
-
-        if(getSharedPreferences("pref1", MODE_PRIVATE).getBoolean("isFirstExecuted", true))
-            db.execSQL("INSERT INTO lock VALUES (1);"); //custom On
-
-        db.close();
-
-        setActivityOrder();
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED|WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD);
 
         findViewById(R.id.btn_on).setOnClickListener(this);
-        findViewById(R.id.btn_off).setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-        Intent intent = new Intent(this, ScreenService.class);
 
         SQLiteDatabase db = new DBHelper(this).getWritableDatabase();
 
         switch (v.getId()) {
             case R.id.btn_on:
-                startService(intent);
-                Toast.makeText(this, "On", Toast.LENGTH_SHORT).show();
-                db.execSQL("UPDATE lock SET custom = 1;");
-                break;
-
-            case R.id.btn_off:
-                stopService(intent);
-                Toast.makeText(this, "Off", Toast.LENGTH_SHORT).show();
-                db.execSQL("UPDATE lock SET custom = 0;");
+                finish();
                 break;
         }
 
@@ -76,56 +63,48 @@ public class LockActivity extends Activity implements OnClickListener{
         PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
         if(!pm.isScreenOn()) {
 
-            SQLiteDatabase db = new DBHelper(getApplicationContext()).getReadableDatabase();
-
-            Cursor c = db.rawQuery("SELECT custom FROM lock", null);
-
-            while(c.moveToNext()) {
-
-                switch (c.getInt(0)) {
-                    case 0:
-                        getWindow().clearFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED|WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD);
-                        Toast.makeText(this, "Custom : "+c.getInt(0), Toast.LENGTH_SHORT).show();
-                        break;
-
-                    case 1:
-                        getWindow().addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED|WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD);
-                        Toast.makeText(this, "Custom : "+c.getInt(0), Toast.LENGTH_SHORT).show();
-                        break;
-                }
-            }
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED|WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD);
             ActivityManager activityManager = (ActivityManager) getApplicationContext()
                     .getSystemService(Context.ACTIVITY_SERVICE);
 
             if (activityManager != null)
                 activityManager.moveTaskToFront(getTaskId(), 0);
-            db.close();
         }
     }
 
     @Override
     protected void onPause() {
-        setActivityOrder();
         super.onPause();
+        ActivityManager activityManager = (ActivityManager) getApplicationContext()
+                .getSystemService(Context.ACTIVITY_SERVICE);
+
+        activityManager.moveTaskToFront(getTaskId(), 0);
+    }
+
+    /*@Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (!hasFocus) {
+            windowCloseHandler.post(windowCloserRunnable);
+        }
+    }
+
+    private void toggleRecents() {
+        Intent closeRecents = new Intent("com.android.systemui.recent.action.TOGGLE_RECENTS");
+        closeRecents.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS);
+        ComponentName recents = new ComponentName("com.android.systemui", "com.android.systemui.recent.RecentsActivity");
+        closeRecents.setComponent(recents);
+        this.startActivity(closeRecents);
     }
 
     @Override
     protected void onUserLeaveHint() {
         finish();
         super.onUserLeaveHint();
-    }
+    }*/
 
     @Override
     public void onBackPressed() {
 
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-
-        SharedPreferences.Editor edit = getSharedPreferences("pref1", MODE_PRIVATE).edit();
-        edit.putBoolean("isFirstExecuted", false);
-        edit.apply();
     }
 }
