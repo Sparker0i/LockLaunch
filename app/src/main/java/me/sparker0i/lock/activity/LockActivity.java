51b995d523522a4f7b297f2a7ac704ba9bcddaa1
abhilal007 <abhilal007@gmail.com>
2017-11-04 21:29:01 +0530

    unresolved changes

--- app/src/main/java/me/sparker0i/lock/activity/LockActivity.java
+++ app/src/main/java/me/sparker0i/lock/activity/LockActivity.java
@@ -1,13 +1,18 @@
 package me.sparker0i.lock.activity;
 
+import android.Manifest;
 import android.app.Activity;
 import android.app.ActivityManager;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.database.sqlite.SQLiteDatabase;
 import android.databinding.DataBindingUtil;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
 import android.util.Log;
 import android.view.View;
@@ -15,6 +20,9 @@ import android.view.View.OnClickListener;
 import android.view.WindowManager;
 import android.widget.RadioButton;
 import android.widget.TextView;
+
+import com.github.florent37.camerafragment.CameraFragment;
+import com.github.florent37.camerafragment.configuration.Configuration;
 
 import java.util.List;
 
@@ -26,16 +34,12 @@ import me.sparker0i.question.database.DatabaseHandler;
 import me.sparker0i.question.model.Question;
 
 @SuppressWarnings("deprecation")
-public class LockActivity extends Activity implements OnClickListener{
+public class LockActivity extends AppCompatActivity {
 
     ActivityLockBinding binding;
-    RadioButton optA , optB , optC , optD;
-    CardView incorrect;
-    TextView quesText;
-    Question question;
     Context context;
-    static DatabaseHandler db;
     Handler handler;
+    Permissions per;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,77 +47,34 @@ public class LockActivity extends Activity implements OnClickListener{
         context = this;
         handler = new Handler();
         binding = DataBindingUtil.setContentView(this , R.layout.activity_lock);
-        optA = binding.radio1;
-        optB = binding.radio2;
-        optC = binding.radio3;
-        optD = binding.radio4;
-        incorrect = binding.incorrect;
-        quesText = binding.question;
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED|WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD);
-        findViewById(R.id.btn_on).setOnClickListener(this);
-        db = new DatabaseHandler(this);
-        try {
-            Log.i("Started" , "Background Thread");
-            question = new BackgroundThread().doInBackground();
-        }
-        catch (Exception ex) {
-            ex.printStackTrace();
-        }
-        handler.post(new Runnable() {
-            @Override
-            public void run() {
-                quesText.setText(question.getQN());
-                optA.setText(question.getA());
-                optB.setText(question.getB());
-                optC.setText(question.getC());
-                optD.setText(question.getD());
-            }
-        });
         Launcher.setLocked(true);
+        per = new Permissions(this);
+        if (Build.VERSION.SDK_INT >= 23)
+            requestPermissions(new String[]{Manifest.permission.CAMERA},20);
+
     }
 
     @Override
-    public void onClick(View v) {
-        SQLiteDatabase db = new DBHelper(this).getWritableDatabase();
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode){
+            case 20:
+                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    CameraFragment cameraFragment = CameraFragment.newInstance(new Configuration.Builder().build());
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.fragment, cameraFragment, "hh")
+                            .commit();
+                }
+                else{
+                    per.permissionDenied();
+                }
 
-        switch (v.getId()) {
-            case R.id.btn_on:
-                checkAnswer(binding.ansGroup.getCheckedRadioButtonId());
-                break;
-        }
-
-        db.close();
-    }
-
-    private void checkAnswer(int selected) {
-        switch(question.getANS()) {
-            case "a" :
-                check(selected , optA);
-                break;
-            case "b" :
-                check(selected , optB);
-                break;
-            case "c" :
-                check(selected , optC);
-                break;
-            case "d" :
-                check(selected , optD);
-                break;
         }
     }
 
     private void unlock() {
-        incorrect.setVisibility(View.INVISIBLE);
         Launcher.setLocked(false);
         finish();
-        Log.i("Answer" , question.getANS());
-    }
-
-    private void check(int selected , RadioButton button) {
-        if (selected == button.getId())
-            unlock();
-        else
-            incorrect.setVisibility(View.VISIBLE);
     }
 
     @Override
@@ -134,13 +95,5 @@ public class LockActivity extends Activity implements OnClickListener{
     @Override
     public void onBackPressed() {
 
-    }
-
-    private static class BackgroundThread extends AsyncTask<Void , Void , Question> {
-
-        @Override
-        protected Question doInBackground(Void... params) {
-            return db.getRandomQuestion();
-        }
     }
 }
